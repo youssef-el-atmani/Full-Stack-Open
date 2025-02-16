7f43a750dcf13ce42672e7768f4cf6733c72c78b
Youssef El Atmani <shihaabyoussef1447@gmail.com>
2025-02-16 22:10:44 +0000

    Put the whole App data into a single object

--- part-1/courseinfo/src/App.jsx
+++ part-1/courseinfo/src/App.jsx
@@ -38,29 +38,30 @@ const Total = ({parts})=> {
 
 // App
 const App = () => {
-  const course = 'Half Stack application development'
-
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    }
-  ]  
-  return (
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+    return (
     <div>
-      <Header courseName = {course} />
+      <Header courseName = {course.name} />
 
-      <Content parts = {parts}/>
+      <Content parts = {course.parts}/>
 
-      <Total parts = {parts}/>
+      <Total parts = {course.parts}/>
     </div>
   )
 }
