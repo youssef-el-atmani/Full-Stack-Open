5368d7b617f08cebd2afaaf1ba9f0cb44b327ec0
Youssef El Atmani <shihaabyoussef1447@gmail.com>
2025-02-16 18:08:45 +0000

    Meet the requirements of part1-a for courseinfo ex
    Do the tasks required for courseinfo app that was listed in part1-a

--- part-1/courseinfo/src/App.jsx
+++ part-1/courseinfo/src/App.jsx
@@ -1,3 +1,41 @@
+// Header
+const Header = ({courseName})=>{
+  return (
+    <h1>{courseName}</h1>
+  )
+  
+}
+
+// Content
+const Part1 = ({part1, exercises1})=> {
+  return <p>{part1} {exercises1}</p>
+}
+
+const Part2 = ({part2, exercises2})=> {
+  return <p>{part2} {exercises2}</p>
+}
+
+const Part3 = ({part3, exercises3})=> {
+  return <p>{part3} {exercises3}</p>
+}
+
+const Content = ({part1, exercises1, part2, exercises2, part3, exercises3})=> {
+  return (
+    <>
+      <Part1 part1={part1} exercises1={exercises1}/>
+      <Part2 part2={part2} exercises2={exercises2}/>
+      <Part3 part3={part3} exercises3={exercises3}/>
+    </>
+  )
+
+}
+
+// Total
+const Total = ({exercises1, exercises2, exercises3})=> {
+  return       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+}
+
+// App
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +47,22 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header courseName = {course} />
+
+      <Content 
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+
+      <Total 
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
   )
 }
